151b172fd25f19fef1fbe93fb71ec7ebb2e841b2
rawandwikat <35597522+rawandwikat@users.noreply.github.com>
2018-04-06 12:16:34 +0300

    Update l.java

--- src/let/l.java
+++ src/let/l.java
@@ -4,8 +4,13 @@ public class l {
 
 	public static void main(String[] args) {
 		int x=0;
-		if(x=0){
-			System.out.println("hello");
+		if(x==0){
+			x=x+1;
+			System.out.println("done");
+			else{
+				x=x+10;
+				System.out.println("done");
+			}
 		}
 
 	}
